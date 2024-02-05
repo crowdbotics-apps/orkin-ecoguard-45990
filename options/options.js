f527c8bbc6bd0cccf74f9c3eb8871b9cf1a3bf05
Crowdbotics <team@crowdbotics.com>
2024-02-05 20:57:00 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "orkin-ecoguard-45990",
+  url: "https://orkin-ecoguard-45990.botics.co",
+  api: "https://orkin-ecoguard-45990.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
